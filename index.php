319c416b370efe880008f37e9e5844c9cb617b5c
medleew <nadir.medlee@gmail.com>
2018-07-30 16:34:18 +0200

    navigation du site et bordure balise html et style

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                             <li><a href="#">Contact</a></li>
                         </ul>
                     </div>
-                    <div>
+                    <div class="objectReparee">
                         <img src="" alt="">
                         <p></p>
                         <p>
