99f9dd15ec09681fc023c41194cc6cf6798cbb46
medleew <nadir.medlee@gmail.com>
2018-07-30 16:03:51 +0200

    Ajout de la page d'acceuil avec un style au min

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- link css style sheet -->
+    <link rel="stylesheet" href="style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+      <?php 
+        include('header.php'); 
+      ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="navigationSiteFr">
+                        <ul class="mt-5">
+                            <li> <a href="index.php">Acceuil</a></li>
+                            <li><a href="#">Media</a></li>
+                            <li><a href="#">Vos Questions</a></li>
+                            <li><a href="#">Nous soutenir</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <p></p>
+                        <p>
+                        objets réparés !
+                        </p>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <h3>Jeter ? Pas question !</h3>
+                    <p>
+                        Réparer ensemble, c’est l’idée des Repair Cafés : des experts en 
+                        la matière sont aussi au rendez-vous. <br/>
+                        <span>Chaque deuxième dimanche du mois</span>, nous vous accueillons au Repair
+                        Café Schaerbeek de 14 à 17h! 
+                    </p>
+                    <h3>Prochaine session: Dimanche 12 août dès 14h au <a href="https://www.google.be/maps?q=Rue+des+Coteaux+307,+Schaerbeek&hl=fr&ll=50.864749,4.387836&spn=0.029361,0.065575&sll=50.8387,4.363405&sspn=0.235021,0.524597&oq=rue+des+cotteaux+307&hnear=Rue+des+Coteaux+307,+Schaerbeek+1030+Schaerbeek,+Bruxelles&t=m&z=14">307, rue des Coteaux à Schaerbeek.</a></h3>
+                    <p>
+                        Avant de vous rendre au Repair Café, nous vous proposons de lire <a href="https://sites.google.com/site/repaircafeschaerbeek/fr/conseils">ces quelques conseils</a>
+                        afin de mieux préparer votre <br/> visite et de donner plus de chance à la réparation de pouvoir
+                        être faite ! Une question ? <br/> Notre rubrique de <a href="https://sites.google.com/site/repaircafeschaerbeek/fr/faq">question-réponse</a> est là pour vous aide
+                    </p>
+                    <h3>Nos ateliers : </h3>
+                    <p>
+                        Couture / Vêtements - Collage / Bois / Meubles - Appareils électriques <br/>
+                        Petit électroménager - Matériel informatique - PC - Imprimantes - Configuration tablettes / smartphones Android <br/>
+                        Bicyclettes - Atelier rémoulage (couteaux de cuisine, ciseaux à bois, sécateurs, lame de tondeuse, <br/>
+                        façonnage de tournevis croqués).
+                    </p>
+                    <h3>Dernières nouvelles</h3>
+                    <p>
+                        Nous recherchons des bénévoles, en particulier pour l'atelier vélo. Contactez-nous.
+                    </p>
+                    <a href="https://www.youtube.com/watch?v=_lWIAb-DW1c">Reportage vidéo Alternatiba</a>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/_lWIAb-DW1c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+      <?php
+        include('footer.php');
+      ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
